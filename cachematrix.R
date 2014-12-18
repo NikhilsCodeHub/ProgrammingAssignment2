09298767966ab91768dc456e61d1092db686ff0b
Nikhil W <wnikhil@gmail.com>
2014-12-18 14:54:55 -0600

    final
    before submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file defines 2 functions to generate and cache Inverse of Matrix.
+##
+## makeCacheMatrix --- Creates a List Object of Matrix and it's Inverse.
+## cacheSolve --- Based on the input Matrix object, checks if Inverse exists. 
+##      If not then calculates the Inverse of the Matrix and caches it.
 
-## Write a short comment describing this function
+
+## makeCacheMatrix --- Creates a List Object of Matrix and it's Inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## Initialize local variable type Matrix to NULL
+    ## Define get, set, getInverse and setInverse function    
+    mInv <- NULL
+    set<- function(mNew) {
+        x<<-mNew
+        mInv<<-NULL
+    }
+    get<- function(){x}
+    setInverse<-function(xInv){
+        mInv<<-xInv
+    }
+    getInverse<-function(){mInv}
+    list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
-
-## Write a short comment describing this function
+ 
+## cacheSolve --- Based on the input Matrix object, checks if Inverse exists. 
+##      If not then calculates the Inverse of the Matrix and caches it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Set local variable to the cached inverse matrix.
+    ## Check if the local variable is NULL
+    ##      If Yes, then calculate the Inverse of matrix and Return result
+    ##          calculate the inverse of Matrix object.
+    ##          Call setInverse to store the Inverse inthe object.
+    ## Return the local variable Matrix that is the inverse of 'x'
+    
+    mInv<-x$getInverse()
+    if(!is.null(mInv)){
+        message("Getting Cached Inverse Matrix.")
+        return(mInv)    
+    }
+    data<-x$get()
+    dataInv<-solve(data)
+    x$setInverse(dataInv)
+    dataInv
 }
